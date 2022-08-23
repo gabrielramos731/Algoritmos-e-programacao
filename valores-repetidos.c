99faf1d7537ac0702d9cb2fe5e2b9ec4382ae3f2
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-08-23 19:54:42 -0300

    add função busca

--- valores-repetidos.c
+++ valores-repetidos.c
@@ -2,12 +2,13 @@
 
 void pastel_ord(int *vet, int tamanho);
 void batata_rep(int *vet, int *vet_rep, int *k);
+int busca(int valor, int *vet_rep, int k);
 
 int main()
 {
               //1,2,2,3,3,3
     int vet[6]={2,1,3,3,2,3}, vet_rep[6];
-    int i, k=0;
+    int i, k=0;   //contador de vet_intersec
 
     pastel_ord(vet,6);
     batata_rep(vet,vet_rep,&k);
@@ -36,19 +37,21 @@ void pastel_ord(int *vet, int tamanho){
 
 //adiciona os valores repetidos em vet_rep
 void batata_rep(int *vet, int *vet_rep, int *k){
-    int i, j, cont=0;
+    int i, j;
     for(i=0;i<6;i++){
-        if(vet[i]==vet[i-1]){   //compara o valor com o valor anterior
-            for(j=0;j<*k;j++){   //varre o vetor de repetidos verificando se vet[i] já esta contido nos repetidos
-                if(vet[i]==vet_rep[j])
-                    cont++;
-            }
-            if(cont==0){
-                vet_rep[*k]=vet[i];   //adiciona ao vetor de repetidos
-                *k+=1;   //posição e tamanho de vet_rep
-            }
-            cont=0;
+        if(vet[i]==vet[i-1] && busca(vet[i],vet_rep,*k)==0){   //compara o valor com o valor anterior e verifica se é repetido
+            vet_rep[*k]=vet[i];   //adiciona ao vetor de repetidos
+            *k+=1;   //posição e tamanho de vet_rep   
         }
     }
 }
-//teste
+
+//verifica se vet[i] já está em vet_inrsec
+int busca(int valor, int *vet_rep, int k){
+    int i;
+    for(i=0;i<k;i++){
+        if(valor==vet_rep[i])
+            return 1;
+    }
+    return 0;
+}
