76f81bbe2f91b72bda939b3da79552572da92b0e
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-08-31 19:16:16 -0300

    funcional

--- binary-search.c
+++ binary-search.c
@@ -3,26 +3,20 @@
 int main(){
 
     int vet[7]={2,4,6,8,10,12,14};
-    int val=5,i=0;
-    int l=0,r=6;
+    int val=14,i=0;
+    int l=0,r=7;
     int mid=(l+r)/2;   //arredonda para baixo
     
-    while(1){
+    while(l<r){
+        mid=(l+r)/2;
         if(vet[mid]==val){
             printf("posicao %d, valor %d encontrado", mid, vet[mid]);
             return 1;
         }
-        else if(vet[mid]<val){
+        else if(vet[mid]<val)
             l=mid+1;
-            mid=(l+r)/2;
-        }
-        else if(vet[mid]>val){
+        else if(vet[mid]>val)
             r=mid-1;
-            mid=(l+r)/2;
-        }
-        else{
-            printf("-1");
-            return 2;
-        }
     }
+    printf("-1");
 }
