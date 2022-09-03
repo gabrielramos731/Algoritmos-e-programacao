231f456299b99b60535a01bc2d40f9ef20250d35
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-09-03 12:53:28 -0300

    questao 5 - a)

--- Estudo dirigido 2 - struct/questao 5.c
+++ Estudo dirigido 2 - struct/questao 5.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+struct dados{
+    char nome[30];
+    int matricula;
+    float nota1, nota2, nota3;
+};
 
 int main(){
-    
+    struct dados dados_alunos[3];
+
+    entrada_dados(dados_alunos, 3);
+
+}
+
+void entrada_dados(struct dados *dados_alunos, int n){
+    int i;
+    for(i=0;i<n;i++){
+        gets(dados_alunos[i].nome);
+        scanf("%d", &dados_alunos[i].matricula);
+        scanf("%f %f %f%*c", &dados_alunos[i].nota1, &dados_alunos[i].nota2, &dados_alunos[i].nota3);
+    }
 }
