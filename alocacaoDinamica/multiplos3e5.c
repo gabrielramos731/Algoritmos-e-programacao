93274081060551c6cc5def268fe063ad7be71857
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-09-20 17:54:36 -0300

    Working

--- alocacaoDinamica/multiplos3e5.c
+++ alocacaoDinamica/multiplos3e5.c
@@ -13,26 +13,23 @@ int main(){
     vet = (int *) calloc(n, sizeof(int));
     for(i=0; i<n; i++)
         scanf("%d", &vet[i]);
-    //printf("%d", vet[1]);
+
     vetMult = multiplos(vet, n, &cont);
-    //for(i=0; i<cont; i++)
-    //    printf("%d ", vetMult[i]);
+    for(i=0; i<cont; i++)
+        printf("%d ", vetMult[i]);
 }
 
 void * multiplos(int *vet, int n, int *cont){
     int i;
     int *vetMultF;
-    for(i=0; i<n; i++){
-        if(vet[i]%3 == 0 && vet[i]%5 == 0){
-            *cont+=1;
-        }
-    }
+    
     vetMultF = (int *) calloc(*cont, sizeof(int));
     for(i=0; i<n; i++){
         if(vet[i]%3 == 0 && vet[i]%5 == 0){
-            vetMultF[i] = vet[i];
+            vetMultF[*cont] = vet[i];
+            *cont+=1;
         }
-    printf("%d %d %d", vetMultF[0], vetMultF[1], vetMultF[2]);
-    return vetMultF;
     }
+    
+    return vetMultF;
 }
