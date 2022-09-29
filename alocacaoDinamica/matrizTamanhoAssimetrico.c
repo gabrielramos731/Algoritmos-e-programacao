c943f4cb6a4d471ec944848bcd30e1d6560ea7cc
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-09-29 16:44:14 -0300

    Update matrizTamanhoAssimetrico.c

--- alocacaoDinamica/matrizTamanhoAssimetrico.c
+++ alocacaoDinamica/matrizTamanhoAssimetrico.c
@@ -6,16 +6,31 @@ void **alocaMatrizAssimetrica(int *vet, int tamanhoVet);
 int main(){
     int *vet, tamanhoVet;
     int **mat;
-    int i;
+    int i, j;
 
-    printf("Qual o tamanho do vetor: ");
+    printf("Qual o tamanho do vetor (num. de linhas): ");
     scanf("%d", &tamanhoVet);
 
     vet = (int *) calloc(tamanhoVet, sizeof(int));
-    printf("Preencha o vetor: ");
+    printf("Preencha o vetor (colunas por linha): ");
     for(i=0; i<tamanhoVet; i++)
         scanf("%d", &vet[i]);
     mat = alocaMatrizAssimetrica(vet,tamanhoVet);
+
+    //preenche a matriz
+    for(i=0; i<tamanhoVet; i++){
+        for(j=0; j<vet[i]; j++){
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    //imprime a matriz
+    for(i=0; i<tamanhoVet; i++){
+        for(j=0; j<vet[i]; j++){
+            printf("%2d ", mat[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 void **alocaMatrizAssimetrica(int *vet, int tamanhoVet){
