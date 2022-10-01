4f108fd84b128f62efdeae4f2f7002bb537a9474
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-09-30 21:24:21 -0300

    função sorteiAposta

--- alocacaoDinamica/bingo.c
+++ alocacaoDinamica/bingo.c
@@ -3,15 +3,17 @@
 #include<time.h>
 
 int * lerAposta(int quantApostas);
+int * sorteiaValores(int quantApostas);
 
 int main(){
-    int *valoresApostados;
+    int *valoresApostados, *valoresSorteados;
     int quantApostas;
 
     printf("Quantos numeros deseja apostar: ");
     scanf("%d", &quantApostas);
 
     valoresApostados = lerAposta(quantApostas);
+    valoresSorteados = sorteiaValores(quantApostas);
 }
 
 int * lerAposta(int quantApostas){
@@ -20,7 +22,7 @@ int * lerAposta(int quantApostas){
 
     vetApostas = (int *) calloc(quantApostas, sizeof(int));
     printf("Digite seus numeros (entre 0 e 100): ");
-    for(i=0; i<quantApostas;){
+    for(i=0; i<quantApostas; ){
         scanf("%d", &vetApostas[i]);
         if(vetApostas[i]<0 || vetApostas[i]>100){
             printf("Valor invalido\n");
@@ -29,4 +31,16 @@ int * lerAposta(int quantApostas){
         i++;
     }
     return vetApostas;
+}
+
+int * sorteiaValores(int quantApostas){
+    int *vetSorteados;
+    int i;
+
+    srand(time(NULL));
+    vetSorteados = (int *) calloc(quantApostas, sizeof(int));
+    for(i=0; i<quantApostas; i++)
+        vetSorteados[i] = rand()%101;
+
+    return vetSorteados;
 }
