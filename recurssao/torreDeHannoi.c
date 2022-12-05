99d59b4188ffeaa1b9921d3d612d2e393ada878b
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-12-05 10:11:31 -0300

    Create torreDeHannoi.c

---
+++ recurssao/torreDeHannoi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+  int n = 4;
+  int quantPassos;
+
+  quantPassos = moverDiscos(n);
+  printf("%d", quantPassos);
+}
+
+int moverDiscos(int n){
+
+  if(n == 1)
+    return 1;
+  else
+    return 1 + (2*moverDiscos(n-1));
+}
